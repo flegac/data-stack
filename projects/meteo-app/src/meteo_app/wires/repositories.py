e9b6e6313c7aec46ebaac338f485859497a838f3
flegac <florent.legac@gmail.com>
2025-05-09 10:09:30 +0200

    fix: minor fixes

--- projects/meteo-app/src/meteo_app/wires/repositories.py
+++ projects/meteo-app/src/meteo_app/wires/repositories.py
@@ -1,19 +1,24 @@
 from dependency_injector import containers, providers
+
 from influxdb_measure_repository.influxdb_measure_repository import (
     InfluxDbMeasureRepository,
 )
+from kafka_connector.kafka_connection import KafkaConnection
 from kafka_message_queue.kafka_factory import KafkaMQBackend
+from meteo_app.wires.config import Config
 from pg_meteo_adapters.data_file_repository import PgDataFileRepository
 from pg_meteo_adapters.ws_repository import PgWorkspaceRepository
+from s3_connector.s3_connection import S3Connection
 from s3_file_repository.s3_file_repository import S3FileRepository
-
-from meteo_app.wires.config import Config
 
 
 # pylint: disable=too-few-public-methods
 class Repositories(containers.DeclarativeContainer):
     config = providers.Container(Config)
-    mq_factory = providers.Singleton(KafkaMQBackend, config=config.kafka)
+
+    kafka_connection = providers.Singleton(KafkaConnection, config=config.kafka)
+
+    mq_factory = providers.Singleton(KafkaMQBackend, connection=kafka_connection)
 
     ws_repository = providers.Singleton(
         PgWorkspaceRepository, database_url=config.database_url
@@ -23,8 +28,10 @@ class Repositories(containers.DeclarativeContainer):
         PgDataFileRepository, database_url=config.database_url
     )
 
+    s3_connection = providers.Singleton(S3Connection, config=config.s3)
+
     file_repository = providers.Singleton(
-        S3FileRepository, config=config.s3, local_path=config.local_path
+        S3FileRepository, connection=s3_connection, local_path=config.local_path
     )
 
     measure_repository = providers.Singleton(
